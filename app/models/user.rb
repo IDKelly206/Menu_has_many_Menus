84b0b9c7aa31390f5eb7b0b748327f4cc6271259
Ian Kelly <idk13@hotmail.com>
2023-06-01 11:09:28 +0200

    sign-up now creates Household and assigns to new User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,18 @@
 class User < ApplicationRecord
-  belongs_to :household
-  accepts_nested_attributes_for :household
-
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
 
-  def with_household
-    self.household.build
-    self
+  belongs_to :household
+
+  before_validation :assign_household
+
+  def assign_household
+    Household.create
+    self.household_id = Household.last.id
+    user = self
+    user.household.name = " #{user.name_last.capitalize} + household"
   end
 
 end
