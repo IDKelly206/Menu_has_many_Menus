97e19795c0f7c7876412b2031790c307c79a45cb
Ian Kelly <idk13@hotmail.com>
2023-06-29 18:18:38 +0200

    finished JS value identification for targets of New Meal form

--- app/javascript/controllers/meal_new_controller.js
+++ app/javascript/controllers/meal_new_controller.js
@@ -2,11 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="meal-new"
 export default class extends Controller {
-  static targets = ["setType", "setUser", "addMeal"]
+  static targets = ["setType", "setUserID", "setMenuID", "addMeal"]
   connect() {
     console.log("Meal New controller")
     console.log(this.setTypeTarget.firstElementChild.value)
-    console.log(this.setUserTarget.value)
+    console.log(this.setUserIDTarget.querySelectorAll("input[type='checkbox'][checked='checked']").forEach(element => console.log(element.value)))
+    console.log(this.setMenuIDTarget.querySelectorAll("input[type='checkbox'][checked='checked']").forEach(element => console.log(element.value)))
   }
 
 
