f32d30d48cf6297536524873f25efe51284a43aa
Ian Kelly <idk13@hotmail.com>
2023-06-30 11:16:04 +0200

    got stimulus targets working and inserting into new form. need to fix checkbox

--- app/javascript/controllers/meal_new_controller.js
+++ app/javascript/controllers/meal_new_controller.js
@@ -2,12 +2,38 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="meal-new"
 export default class extends Controller {
-  static targets = ["setType", "setUserID", "setMenuID", "addMeal"]
+  static targets = [ "getMenuID", "getUserID", "getType", "setMenuID", "setUserID", "setType"]
   connect() {
     console.log("Meal New controller")
-    console.log(this.setTypeTarget.firstElementChild.value)
-    console.log(this.setUserIDTarget.querySelectorAll("input[type='checkbox'][checked='checked']").forEach(element => console.log(element.value)))
-    console.log(this.setMenuIDTarget.querySelectorAll("input[type='checkbox'][checked='checked']").forEach(element => console.log(element.value)))
+    console.log(this.getMenuIDTarget.querySelectorAll("input[type='checkbox'][checked='checked']").forEach(element => console.log(element.value)))
+    console.log(this.getUserIDTarget.querySelectorAll("input[type='checkbox'][checked='checked']").forEach(element => console.log(element.value)))
+    console.log(this.getTypeTarget.firstElementChild.value)
+    console.log(this.setTypeTarget.value)
+
+  }
+
+  setMeal(event) {
+    event.preventDefault()
+    this.setMenuIDTarget.value = this.menuIDs;
+    this.setUserIDTarget.value = this.userIDs;
+    this.setTypeTarget.value = this.mealType;
+  }
+
+  get menuIDs() {
+    let menuIDs = []
+    this.getMenuIDTarget.querySelectorAll("input[type='checkbox'][checked=true]").forEach(element => menuIDs.push(element.value))
+    return menuIDs
+  }
+
+  get userIDs() {
+    let userIDS = []
+    this.getUserIDTarget.querySelectorAll("input[type='checkbox'][checked='checked']").forEach(element => userIDS.push(element.value))
+    return userIDS
+  }
+
+  get mealType() {
+    let mealType = this.getTypeTarget.firstElementChild.value
+    return mealType
   }
 
 
