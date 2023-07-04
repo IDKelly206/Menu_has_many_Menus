d4300e6a01b236e9e8954fbb0e1812262b2e6e15
Ian Kelly <idk13@hotmail.com>
2023-07-04 08:46:04 +0200

    corrected meal model for dependent destroy of courses

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,7 +2,7 @@ class Meal < ApplicationRecord
   belongs_to :menu
   belongs_to :user
 
-  has_many :courses, dependent: :delete
+  has_many :courses, dependent: :destroy
 
   validates :meal_type, presence: true
 
