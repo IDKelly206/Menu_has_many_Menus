556b7a48025f4f813fdc2dbec8b82d2530bbbc4e
Ian Kelly <idk13@hotmail.com>
2023-07-12 20:59:27 +0200

    minor updates

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,16 @@
 class RecipesController < ApplicationController
+
+  require "json"
+  require "rest-client"
+
+
   def index
 
   end
 
   def new
-    response = HTTParty.get("https://api.edamam.com/api/recipes/v2?type=public&q=chicken%20&app_id=bb5e4702&app_key=7cb8c06cdedbc2d089957cc57703423c&mealType=Dinner&imageSize=REGULAR")
-    parsed = JSON.parse(response.body)["hits"]
-    
-    console
+    search_results = HTTParty.get("https://api.edamam.com/api/recipes/v2?type=public&q=chicken%20&app_id=bb5e4702&app_key=7cb8c06cdedbc2d089957cc57703423c&mealType=Dinner&imageSize=REGULAR")
+    @recipes = JSON.parse(search_results.body)["hits"]
+
   end
 end
