cfa6b2a3dd9c854046281a05249556d519641c38
Ian Kelly <idk13@hotmail.com>
2024-02-06 18:31:56 +0100

    add attr hash to product_img in Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,10 @@
 class Product < ApplicationRecord
 
   # Select Kroger product image perspective and size & return url
-  def kroger_img(perspective, size )
-    front_img = self.images.select { |p| p["perspective"] == "#{perspective}" }
-    front_img.first["sizes"].select { |s| s["size"] == "#{size}" }.first["url"]
+  def product_img(attrs = {})
+    per = attrs[:perspective]
+    sze = attrs[:size]
+    images = self.images.select { |p| p["perspective"] == per }
+    images.first["sizes"].select { |s| s["size"] == sze }.first["url"]
   end
 end
