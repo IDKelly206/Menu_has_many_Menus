160c5674e44b5ff078fd7caf158e66613f5ab757
Ian Kelly <idk13@hotmail.com>
2024-02-07 11:56:03 +0100

    add default to product_image selector in case of nil

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,16 @@ class Product < ApplicationRecord
   # Select Kroger product image perspective and size & return url
   def product_img(attr = {})
     per = attr[:perspective].downcase
+    per_default = "front"
     sz = attr[:size]
-    perscpective_images = self.images.detect { |p| p["perspective"] == per }
+    # sz_default = "thumbnail"
+
+    if !images.detect { |p| p["perspective"] == per }.nil?
+      perscpective_images = images.detect { |p| p["perspective"] == per }
+    else
+      perscpective_images = images.detect { |p| p["perspective"] == per_default }
+    end
+
     image = perscpective_images["sizes"].detect { |s| s["size"] == sz }
     image["url"]
   end
