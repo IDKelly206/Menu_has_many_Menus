c0ae29ecfb97adb1ea522d475b9cb0622ccc0b96
Ian Kelly <idk13@hotmail.com>
2024-02-07 10:40:18 +0100

    refactor product_image selector

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,10 @@ class Product < ApplicationRecord
 
   # Select Kroger product image perspective and size & return url
   def product_img(attr = {})
-    per = attr[:perspective]
+    per = attr[:perspective].downcase
     sz = attr[:size]
-    images = self.images.select { |p| p["perspective"] == per }
-    images.first["sizes"].select { |s| s["size"] == sz }.first["url"]
+    perscpective_images = self.images.detect { |p| p["perspective"] == per }
+    image = perscpective_images["sizes"].detect { |s| s["size"] == sz }
+    image["url"]
   end
 end
