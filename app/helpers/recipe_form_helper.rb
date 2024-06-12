d3b406a86b9024f7bf99e1560b6aaf1fd7d6913e
Ian Kelly <idk13@hotmail.com>
2024-06-12 20:45:52 +0200

    recipe card text over img background

--- app/helpers/recipe_form_helper.rb
+++ app/helpers/recipe_form_helper.rb
@@ -1,12 +1,12 @@
 module RecipeFormHelper
   # Inserts correct form into recipe card
-  def recipe_form(attrs = { })
+  def recipe_form(attrs = {})
     page_title = attrs[:page_title]
     erecipe_id = attrs[:erecipe_id]
     if page_title == "Course" || "Meal Planner"
       render 'courses/form', erecipe_id: erecipe_id
     elsif page_title == "Recipes"
-      # "More details"
+      "More details"
     else
       "BLANK Footer Area"
     end
