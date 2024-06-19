a5d70c2b8e442b796ae88c1caddf2fe0c0a551f1
Ian Kelly <idk13@hotmail.com>
2024-06-19 17:37:45 +0200

    add visability options to stim controller and apply various transition to recipe card

--- app/javascript/controllers/visability_controller.js
+++ app/javascript/controllers/visability_controller.js
@@ -2,14 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="visability"
 export default class extends Controller {
-  static targets = [ "hideable" ]
+  static targets = [ "hideableDiv", "hideableFlex" ]
 
   connect() {
     console.log("Visability controller")
-    console.dir(this.hideableTargets)
   }
 
-  showTarget(el) {
+  showDivTarget(el) {
+    // Get natural height of element
+    var getHeight = function () {
+      el.style.display = 'block'; //Make it visible
+      var height = el.scrollHeight + 'px'; //Get it's height
+      el.style.display = ''; //Hide display again
+      return height
+    }
+    var height = getHeight(); //Get natural height
+    el.classList.remove("div-hidden"); // Make element visible
+    el.style.height = height; // Update max-height
+    window.setTimeout(function () {
+      el.style.height = '';
+    }, 1500);
 
   // // Attemp (2nd) [is-oepn] using hidden + transform(translateY())
     // el.removeEventListener('transitionend', listener);
@@ -17,28 +29,19 @@ export default class extends Controller {
     // el.removeAttribute('hidden')
     // const reflow = el.offsetHeight;
     // el.classList.add('is-open')
-
-
-    // // Attempt (1st) [is-hidden] using height set and decrease
-    // // Get natural height of element
-    // var getHeight = function () {
-    //   el.style.display = 'block'; //Make it visible
-    //   var height = el.scrollHeight + 'px'; //Get it's height
-    //   el.style.display = ''; //Hide display again
-    //   return height
-    // }
-    // var height = getHeight(); //Get natural height
-    // el.classList.remove('is-hidden'); // Make element visible
-    // el.style.height = height; // Update max-height
-    // window.setTimeout(function () {
-    //   el.style.height = '';
-    // }, 1000);
-
   };
 
-
-  hideTarget(el) {
-
+  hideDivTarget(el) {
+    // Attempt (1st) [div-hidden] using height set and decrease
+    el.style.height = el.scrollHeight + 'px'; // Give # to change from
+    // Set height back to zero
+    window.setTimeout(function() {
+      el.style.height = '0';
+    }, 1500);
+    //When transition compelte add class div-hidden
+    window.setTimeout(function () {
+      el.classList.add("div-hidden");
+    }, 1500);
 
     // // Attemp (2nd) [is-oepn] using hidden + transform(translateY())
     // listener(el) {
@@ -48,25 +51,26 @@ export default class extends Controller {
     // };
     // el.setAttribute('hidden', true)
     // el.classList.remove('is-open')
-
-    // // Attempt (1st) [is-hidden] using height set and decrease
-    // el.style.height = el.scrollHeight + 'px'; // Give # to change from
-
-    // // Set height back to zero
-    // window.setTimeout(function() {
-    //   el.style.height = '0';
-    // }, 10);
-
-    // //When transition compelte add class is-hidden
-    // window.setTimeout(function () {
-    //   el.classList.add('is-hidden');
-    // }, 700);
   };
 
   toggleTargets() {
-    console.log("Action - toggle")
-    this.hideableTargets.forEach((el) => {
-      el.classList.toggle("collapsed")
+    // console.log("Action - toggle")
+
+    // Div collapse toggle
+    // target: hideableDiv & class: hideable-div & div-hidden
+    this.hideableDivTargets.forEach((el) => {
+      if (el.classList.contains("div-hidden")) {
+        this.showDivTarget(el)
+        return;
+      }
+      this.hideDivTarget(el);
+    });
+
+    // Flexbox collapse toggle
+    // target: hideableFlex & class: hideable-flex + flex-collapsed
+    this.hideableFlexTargets.forEach((el) => {
+      el.classList.toggle("flex-collapsed")
+    });
 
 
       // // Attemp (2nd) [is-oepn] using hidden + transform(translateY())
@@ -77,16 +81,5 @@ export default class extends Controller {
       //   return;
       // }
       // this.showTarget(el)
-
-      // // Attempt (1st) [is-hidden] using height set and decrease
-      // if (el.classList.contains('is-hidden')) {
-      //   this.showTarget(el)
-      //   return;
-      // } else {
-      //   this.hideTarget(el);
-      // }
-
-    });
-
   }
 }
