e0e24a83af0a6be0ec3c6e2041ac507cf9277bec
Ian Kelly <idk13@hotmail.com>
2024-08-12 13:03:43 +0200

    create filter component for styling and apply to sidebar

--- app/helpers/filter_helper.rb
+++ app/helpers/filter_helper.rb
@@ -1,11 +1,10 @@
 module FilterHelper
-
-  #  Pre-filtered selection for search
+  # Filtered selection applied to recipe search
   def filter_types(attr = {})
     unless attr[:type].nil?
       type = attr[:type]
       type = type.capitalize
-      pills = "<li class='btn--pill btn--pill--meal'>#{type}</li>"
+      pills = "<li class='btn--pill btn--pill-meal'>#{type}</li>"
       pills.html_safe
     end
   end
@@ -13,11 +12,11 @@ module FilterHelper
   def filter_users(attr = {})
     unless attr[:users].nil?
       users = attr[:users]
-      if users.class == Array
-        pills = users.map { |u| "<li class='btn--pill btn--pill--user'>#{u.name_first}</li>" }
+      if users.instance_of? Array
+        pills = users.map { |u| "<li class='btn--pill btn--pill-user'>#{u.name_first}</li>" }
         pill = pills.join(' ')
       else
-        pill = "<li class='btn--pill btn--pill--user'>#{users.name_first}</li>"
+        pill = "<li class='btn--pill btn--pill-user'>#{users.name_first}</li>"
       end
       pill.html_safe
     end
@@ -26,11 +25,11 @@ module FilterHelper
   def filter_dates(attr = {})
     unless attr[:dates].nil?
       menus = attr[:dates]
-      if menus.class == Array
-        pills = menus.map { |m| "<li class='btn--pill btn--pill--date'>#{m.day_of_week}</li>" }
+      if menus.instance_of? Array
+        pills = menus.map { |m| "<li class='btn--pill btn--pill-date'>#{m.day_of_week}</li>" }
         pill = pills.join(' ')
       else
-        pill = "<li class='btn--pill btn--pill--date'>#{menus.day_of_week}</li>"
+        pill = "<li class='btn--pill btn--pill-date'>#{menus.day_of_week}</li>"
       end
       pill.html_safe
     end
@@ -39,7 +38,7 @@ module FilterHelper
   def filter_dietary(attr = {})
     unless attr[:dietary].nil?
       dietary = attr[:dietary]
-      pills = dietary.map { |dr| "<li class='btn--pill btn--pill--dietary'>#{dr.capitalize}</li>" }
+      pills = dietary.map { |dr| "<li class='btn--pill btn--pill-dietary'>#{dr.capitalize}</li>" }
       pill = pills.join(' ')
       pill.html_safe
     end
@@ -55,51 +54,52 @@ module FilterHelper
   #   pill.html_safe
   # end
 
-  # Filtering selection for search
-
-  def select_filter_meal_types(attr = {})
+  # Filtering radio selection for recipe search
+  def select_radio_meal_types(attr = {})
     unless attr[:meal_types].nil?
       meal_types = attr[:meal_types]
       pills = meal_types.map do |mt|
-        "<div class='radio-item'>
-        <input class='radio-btn btn--pill btn--pill--meal' type='radio'
-              name='mealType'
-              id='#{mt}'
-              value='#{mt}'
-              >
-        <label for='#{mt}'
-              # data-action='
-              click->recipe-search-filter#assignMealFilter'
-              >
-              #{mt.capitalize} </label>
-        </div>"
+        "<li class='radio-item'>
+          <input class='radio-btn btn--pill btn--pill-meal' type='radio'
+                name='mealType'
+                id='#{mt}'
+                value='#{mt}'
+                >
+          <label for='#{mt}'
+                data-action='
+                  click->recipe-search-filter#assignMealFilter'
+                >
+                #{mt.capitalize}
+          </label>
+        </li>"
       end
       pill = pills.join(' ')
       pill.html_safe
     end
   end
 
-  def select_filter_course_types(attr = {})
+  def select_radio_course_types(attr = {})
     unless attr[:course_types].nil?
     course_types = attr[:course_types]
     course_type = attr[:course_type].nil? ? nil : attr[:course_type]
 
     pills = course_types.map { |ct|
-        "<div class='radio-item'>
-        <input class='radio-btn btn--pill btn--pill--course' type='radio'
-               name='dishType'
-               id='#{ct}'
-               value='#{ct}'
-
-              data-course-new-form-target='courseRadioInput'
-               >
-        <label for='#{ct}'
-              data-action='
-                           click->course-new-form#assignCourseType
-                           click->recipe-search-filter#assignCourseFilter'
-               >
-               #{ct.capitalize} </label>
-        </div>"
+        "<li class='radio-item'>
+          <input class='radio-btn btn--pill btn--pill-course'
+                type='radio'
+                name='dishType'
+                id='#{ct}'
+                value='#{ct}'
+                data-course-new-form-target='courseRadioInput'
+                >
+          <label for='#{ct}'
+                  data-action='
+                    click->course-new-form#assignCourseType
+                    click->recipe-search-filter#assignCourseFilter'
+                >
+                #{ct.capitalize}
+          </label>
+        </li>"
       }
       pill = pills.join(' ')
       pill.html_safe
