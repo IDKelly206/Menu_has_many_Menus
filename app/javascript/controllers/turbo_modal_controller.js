93ead5297708eb2cf32de0cfaf323f8063486036
Ian Kelly <idk13@hotmail.com>
2024-09-24 13:51:24 +0200

    modal work. centered on screen with shrink to content fit. add background blur to modal. add line to hide modal method in  turbo modal controller to remove overlay.

--- app/javascript/controllers/turbo_modal_controller.js
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -2,17 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="turbo-modal"
 export default class extends Controller {
-  static targets = [ "grocery" ]
+  static targets = [ "grocery", "overlay" ]
 
   connect() {
     console.log("Turbo Modal controller")
     // const planner = document.getElementById("planner")
     // ids = planner.dataset.courseIds
     // this.openTurboFrame(this.idsValue)
+    console.dir(this.element)
   }
 
   hideModal() {
     this.element.parentElement.removeAttribute("src")
+    this.element.nextElementSibling.remove()
     this.element.remove()
   }
 
