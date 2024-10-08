d7afb6599a97be43c4ec98a7d35c8effd9052e6f
Ian Kelly <idk13@hotmail.com>
2024-10-08 10:04:25 +0200

    add dialog box with form and js contoller for turbo confirm

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,14 +5,27 @@ import "bootstrap"
 
 
 
-import { Turbo } from "@hotwired/turbo-rails"
+// import { Turbo } from "@hotwired/turbo-rails"
 // Turbo.session.drive = false
 
 // Redirect out of turbo frame on-click
 // turbo_stream.adv_redirect(XXXX_path )
 Turbo.StreamActions.adv_redirect = function() {
-  // console.log(this.target)
   let url = this.getAttribute('url')
   // console.log(url)
   Turbo.visit(url)
 }
+
+// Confirmation Modal of Turbo
+Turbo.setConfirmMethod((message, element) => {
+  console.log(message, element)
+  let dialog = document.getElementById('turbo-confirm')
+  dialog.querySelector("p").textContent = message
+  dialog.showModal();
+
+  return new Promise((resolve, reject) => {
+        dialog.addEventListener("close", () => {
+              resolve(dialog.returnValue == 'confirm')
+        }, { once: true })
+  })
+})
