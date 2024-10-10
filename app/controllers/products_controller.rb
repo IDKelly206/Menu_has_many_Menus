46b06e3b8a1b244664481df47f5a2fe941d4732a
Ian Kelly <idk13@hotmail.com>
2024-10-10 10:34:34 +0200

    add repsonders gem for devise update to repsond with turbo. still issues with notifications for devise and user#create validation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,9 @@
 class ProductsController < ApplicationController
-
-
   def index
     if params[:query].present? || !params[:query].nil?
       @products = Kroger::KrogerProduct.search(params[:query])
     else
       @products = Kroger::KrogerProduct.search(["tomato"])
     end
-
-    
   end
-
 end
--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ Bundler.require(*Rails.groups)
 
 module PlannerV3
   class Application < Rails::Application
+    # Use the responders controller from the responders gem
+    config.app_generators.scaffold_controller :responders_controller
+
     config.generators do |generate|
       generate.assets false
       generate.helper false
